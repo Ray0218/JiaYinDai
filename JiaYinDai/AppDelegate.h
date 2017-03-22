89c702f3e6e6b451fb51b522179454453ae3892d
Ray0218 <ray_ios@163.com>
2017-03-22 15:37:26 +0800

    Initial Commit

---
+++ JiaYinDai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiaYinDai
+//
+//  Created by 吴孔亮 on 2017/3/22.
+//  Copyright © 2017年 嘉远控股. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JiaYinDaiTests/JiaYinDaiTests.m
@@ -0,0 +1,39 @@
+//
+//  JiaYinDaiTests.m
+//  JiaYinDaiTests
+//
+//  Created by 吴孔亮 on 2017/3/22.
+//  Copyright © 2017年 嘉远控股. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JiaYinDaiTests : XCTestCase
+
+@end
+
+@implementation JiaYinDaiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
